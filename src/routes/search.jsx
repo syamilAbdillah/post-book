74179576f2cfaf7fea21ed39d090e6508ca8e2a9
syamilAbdillah <abdillahsyamil77@gmail.com>
2023-03-21 20:40:43 +0700

    feat: add debounced to search page

--- src/routes/search.jsx
+++ src/routes/search.jsx
@@ -20,29 +20,50 @@ import Section from './components/Section.jsx'
 import TextInput from './components/TextInput.jsx'
 import Avatar from './components/Avatar.jsx'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useDebouncedValue } from '@mantine/hooks'
 
 export default function Search() {
 	return <>
-        <Section>
-            <TextInput type="search" placeholder="search user ..." />
-        </Section>
-        <Section>
-            <div className="flex flex-col gap-4">
-                {users.map(user => (
-                    <UserCard user={user} key={user.id} />
-                ))}
-            </div>
-        </Section>
+        <SearchInput/>
+        <SearchList users={users} />
     </>
 }
 
-const UserCard = ({user}) => (
-    <Link to="#" className="flex items-center gap-4 p-2 hover:bg-base-200 rounded-lg ">
-        <Avatar 
-            size="sm"
-            src={user.avatar} 
-            alt={`${user.username} profile picture`} 
+const SearchInput = () => {
+    const [value, setValue] = useState('')
+    const [debounced] = useDebouncedValue(value, 750)
+
+    useEffect(() => {
+        if(debounced && debounced != '') {
+            console.log(debounced)}
+        }
+    , [debounced])
+
+    return <Section>
+        <TextInput 
+            onChange={ev => setValue(ev.currentTarget.value)} 
+            type="search" 
+            placeholder="search user ..." 
         />
-        <h2 className="text-xl">{user.username}</h2>
-    </Link>
-)
+    </Section>
+}
+
+const SearchList = ({ users }) => {
+    return <Section>
+        <div className="flex flex-col gap-4">
+            {users.map(user => (
+                <UserCard user={user} key={user.id} />
+            ))}
+        </div>
+    </Section>
+}
+
+const UserCard = ({user}) => <Link to="#" className="flex items-center gap-4 p-2 hover:bg-base-200 rounded-lg ">
+    <Avatar 
+        size="sm"
+        src={user.avatar} 
+        alt={`${user.username} profile picture`} 
+    />
+    <h2 className="text-xl">{user.username}</h2>
+</Link>
